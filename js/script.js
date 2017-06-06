d32497d1c93ebfc79f7aab9f5a1f78e618bf7b45
Nikola Stamatovic <nikola@otilito.com>
2017-06-06 19:25:58 +0200

    some styles

--- js/script.js
+++ js/script.js
@@ -26,24 +26,25 @@ $(document).ready(function(){
     	images.push(fname);
   	});
 
-	$('.submitbtn').click(function(ev){
+	var fields = ['class_name', 'class_description','class_type','demo', 'class_location','address1', 'address2', 'city', 'state', 'zip', 'price', 'start_time',
+	'end_time','facebook','instagram', 'twitter', 'seats', 'bio', 'paypal', 'phone'];
 
+	$('.submitbtn').click(function(ev){
+		ev.preventDefault();
 		if ($(this).hasClass('load')) {
 			return;
 		}
-		ev.preventDefault();
+
+		$('#signup .suc').hide();
 
 		var parent = $(this).parent();
 
 		var e = {};
-		e.class_name = $(parent).find('#class_name');
-		e.class_description = $(parent).find('#class_description');
-		e.class_location = $(parent).find('#class_location');
-
 		var o = {};
-		o.class_name = $(e.class_name).val().trim();
-		o.class_description = $(e.class_description).val().trim();
-		o.class_location = $(e.class_location).val().trim();
+		for (var i = 0; i < fields.length; i++) {
+			e[fields[i]] = $(parent).find('#'+fields[i]);
+			o[fields[i]] = e[fields[i]].val().trim();
+		}
 
 		if (hero_image) {
 			o.hero_image = hero_image;
@@ -60,12 +61,23 @@ $(document).ready(function(){
 		function onSuccess(data) {
 			$(self).removeClass('load');
 			if (data.error) {
-				$('#signup .err').show().html(data.error);
+				if (data.hasOwnProperty('type') && data.type === 'validation') {
+					for (var i = 0; i < data.error.length; i++) {
+						var err = data.error[i];
+						$('#'+err.field).parent().find('.err').show().html(err.message);
+					}
+				} else {
+					$('#signup .gen-err').show().html(data.error);
+				}
 				return;
 			}
 			$('#signup .suc').show();
-			$(e.class_name).val('');
-			$(e.class_description).val('');
+			for (var i = 0; i < fields.length; i++) {
+				e[fields[i]].val('');
+			}
+			$('.err').hide().html('');
+			dz.removeAllFiles( true );
+			dza.removeAllFiles( true );
 		}
 
 		$(this).addClass('load');
